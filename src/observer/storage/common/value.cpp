690c387121471a844b98c191c97728e87f5f22b5
ccat3z <fzhang.chn@foxmail.com>
2021-10-22 17:16:20 +0800

    storage: add util: value_cast

---
+++ src/observer/storage/common/value.cpp
@@ -0,0 +1,34 @@
+// Copyright (c) 2021 Lingfeng Zhang(fzhang.chn@foxmail.com). All rights
+// reserved. miniob is licensed under Mulan PSL v2. You can use this software
+// according to the terms and conditions of the Mulan PSL v2. You may obtain a
+// copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the
+// Mulan PSL v2 for more details.
+
+#include "value.h"
+#include "common/time/datetime.h"
+#include <string.h>
+
+bool value_cast(Value *value, AttrType type) {
+  if (value->type == type) return true;
+
+  if (value->type == CHARS && type == DATE) {
+    common::Date date;
+    if (!date.parse((char *) value->data)) {
+      return false;
+    }
+
+    free(value->data);
+
+    value->type = DATE;
+    void *data = malloc(sizeof(int));
+    int julian = date.julian();
+    memcpy(value->data, &julian, sizeof(int));
+    return true;
+  }
+
+  return false;
+}
