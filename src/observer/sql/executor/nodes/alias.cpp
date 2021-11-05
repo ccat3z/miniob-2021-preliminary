97ffd47bf4536978bc9fff5f19a9b8a0bb89661c
ccat3z <fzhang.chn@foxmail.com>
2021-11-05 21:49:12 +0800

    executor: new node: alias

---
+++ src/observer/sql/executor/nodes/alias.cpp
@@ -0,0 +1,30 @@
+#include "alias.h"
+#include "filter.h"
+
+AliasNode::AliasNode(std::unique_ptr<ExecutionNode> child,
+                     const char *table_name)
+    : child(std::move(child)) {
+  for (auto field : this->child->schema().fields()) {
+    tuple_schema_.add(field.type(), table_name, field.field_name());
+  }
+}
+
+AliasNode::~AliasNode() {}
+
+const TupleSchema &AliasNode::schema() { return tuple_schema_; }
+
+RC AliasNode::next(Tuple &tuple) { return child->next(tuple); }
+void AliasNode::reset() { child->reset(); }
+
+std::unique_ptr<ExecutionNode>
+AliasNode::push_down_predicate(std::list<Condition *> &predicate) {
+  auto new_child = child->push_down_predicate(predicate);
+  if (new_child != nullptr) {
+    child = std::move(new_child);
+  }
+  if (predicate.size() == 0) {
+    child = std::make_unique<FilterNode>(std::move(child), predicate.begin(),
+                                         predicate.end());
+  }
+  return nullptr;
+}
---
+++ src/observer/sql/executor/nodes/alias.h
@@ -0,0 +1,31 @@
+// Copyright (c) 2021 Lingfeng Zhang(fzhang.chn@foxmail.com). All rights
+// reserved. miniob is licensed under Mulan PSL v2. You can use this software
+// according to the terms and conditions of the Mulan PSL v2. You may obtain a
+// copy of Mulan PSL v2 at:
+//          http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY
+// KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO
+// NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE. See the
+// Mulan PSL v2 for more details.
+
+#ifndef __OBSERVER_SQL_EXECUTOR_NODES_ALIAS_H_
+#define __OBSERVER_SQL_EXECUTOR_NODES_ALIAS_H_
+
+#include "base.h"
+
+class AliasNode : public VolcanoExecutionNode {
+public:
+  AliasNode(std::unique_ptr<ExecutionNode> child, const char *table_name);
+  virtual ~AliasNode();
+  const TupleSchema &schema() override;
+  RC next(Tuple &tuple) override;
+  void reset() override;
+  std::unique_ptr<ExecutionNode>
+  push_down_predicate(std::list<Condition *> &predicate) override;
+
+private:
+  TupleSchema tuple_schema_;
+  std::unique_ptr<ExecutionNode> child;
+};
+
+#endif // __OBSERVER_SQL_EXECUTOR_NODES_ALIAS_H_
